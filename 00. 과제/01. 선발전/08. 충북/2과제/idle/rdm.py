3489609b8af9c0583e1e406927b5e6ec464c8797
DongHyuk Kim <kdh12210@gmail.com>
2024-10-13 10:14:49 +0900

    rename: 파일 정리.

---
+++ 00. 과제/01. 선발전/08. 충북/2과제/idle/rdm.py
@@ -0,0 +1,11 @@
+import random
+from printer import printf
+def rdm(t):
+    c = random.choice
+    cs = random.sample
+    if t == 0:
+        m = '0x0x00000000000000000x0x0'
+        printf(m,5,5)
+    return res
+
+rdm(0)
