c8be9fbe03e1f2ec6ef7b63bac00774fb9bb5cb0
DongHyukWin <kdh12210@gmail.com>
2025-05-21 11:45:41 +0900

    feat: '24 충남2

---
+++ 00. 선발전/과제/07. 충남/2과제/idle/Tool/Factory.py
@@ -0,0 +1,32 @@
+from Case import Case
+import sys,os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# from release.main_perf2_2 import main      # from 'fileName' import main
+from main import main      # from 'fileName' import main
+Case.main = main
+
+def factory():
+    global case
+    while Case.ct < limit:
+        case = Case(t)
+        res = case.run()
+        if not res:
+            break
+    Case.status()
+
+t = 2
+limit = 100
+Case.mod = 1
+
+Case.printStyle = (" [{}] | {},'{}',{}" if t < 2 else 
+                   " [{}] | {},'{}',{},{}" if Case.mod == 0 else 
+                   " [{}] | {},'{}'" if Case.mod == 1 else 
+                   " [{}] | {},'{}','{}',{}")
+try:
+    print()
+    factory()
+except KeyboardInterrupt:
+    Case.failCase = case
+    Case.err = 'Ctrl + C'
+    Case.status()
+    
