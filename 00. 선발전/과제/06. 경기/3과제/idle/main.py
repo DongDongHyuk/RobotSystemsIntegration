3287c27912e1e6c37e72c6dc6d145cca37b1a4b5
DongHyukWin <kdh12210@gmail.com>
2025-05-24 15:11:33 +0900

    feat: '24 경기3

--- 00. 선발전/과제/06. 경기/3과제/idle/main.py
+++ 00. 선발전/과제/06. 경기/3과제/idle/main.py
@@ -1,26 +1,19 @@
-from collections import deque
 from time import time
-from printer import printf
+from Tool.Printer import *
+from queue import *
 
-def exc(m,s,e=-1,g=-1,p=-1,gli=-1,li=-1):
+def exc(m,s,e,r,p=-1,pk=-1):
     m = list(m)
-    if g != -1:
-        isg = g == 1
-        pack = m[s] if isg else p
-        gp = gli.index('0' if isg else p)
-        gli = list(gli)
-        gli[gp] = pack if isg else '0'
-        m[s] = '0' if isg else p
+    di = {'abc'[i-10] if i > 9 else str(i):i for i in range(1,13)}
+    if p != -1:
+        n = int(pk == -1)
+        info = [n,p,di[m[p] if n else pk]]        # n,p,pk
+        m[p] = '0' if n else pk
     else:
         m[s],m[e] = m[e],m[s]
-        pack = m[s]
-        step = li if li != -1 else [e,s]
-    pack = 'abc'.index(pack)+10 if pack.isalpha() else int(pack)
-    info = [s,g,pack,gp] if g != -1 else [step,pack]
-    if g == -1 and m[e] != '0':
-        info.append(int(m[e]))
-    return [(''.join(m),tuple(gli)) if t == 3 else ''.join(m),info]
-    
+        info = [r,di[m[s]] if m[s] in di else int(m[s])]
+    return [''.join(m),info]
+
 def aro(pos):
     if pos in cache:
         return cache[pos]
@@ -34,176 +27,143 @@ def aro(pos):
     cache[pos] = res
     return res
 
-def exp(n,m,pos=-1):
-    if t == 3:
-        m,gli = m
+def exp(n,m,p=-1,leaf=-1):
     res = []
-    if t == 2:
-        for i in range(8):
-            for j in range(8):
-                if i != j and all([k not in fix for k in [i,j]]):
-                    res.append(exc(m,i,j))
-        return res
-    for i in range(size * sz) if n > 0 else [pos]: 
-        if i in fix:
+    for i in range(size) if n > 0 else [p]:
+        if fx[i] or m[i] == 'x':
             continue
-        if t == 3:
-            if m[i] == '0' and gli.count('0') < 2 and (i > 5 or m[i + 2] == '0') and (i < 3 or m[i - 2] != '0'):        # put a pack
-                for p in gli:
-                    if p != '0':                        
-                        res.append(exc(m,i,g=0,p=p,gli=gli))
-            if gli.count('0') < 1:      # used all gripper
+        if t == 2:
+            if m[i] != '0' and m.count('0') < 4 and (i > 5 or m[i+2]=='0'):
+                res.append(exc(m,-1,-1,-1,i,-1))
+            if m[i] == '0' and (i < 2 or m[i-2] != '0'):
+                for pk in leaf:
+                    if pk not in m:
+                        res.append(exc(m,-1,-1,-1,i,pk))
+            continue
+        if (n > 0 and m[i] == '0'):
+            continue
+        di = src(-1,m,i,-1) if n > 0 else aro(i)
+        for j in di:
+            if j == i or fx[j] or m[j] == 'x' or (n == -1 and m[j] != '0'):
                 continue
-            if m[i] != '0' and (i > 5 or m[i + 2] == '0'):      # get a pack
-                res.append(exc(m,i,g=1,gli=gli))
-            continue
-        if (n == -1 or n > 0) and m[i] != '0':
-            continue
-        for j in [j for j in aro(i) if m[j] not in ['x','0x'][n > 0] and j not in fix]:            
-            res.append(exc(m,i,j) if n > 0 else [j,j])
+            res.append(exc(m,j,i,di[j]) if n > 0 else [j,j])        
     return res
 
-def bfs(n,m,*a):
-    global res
-    if n == -1:
-        s, = a
-    if n == 0:
+mkdCt = 0
+def src(n,m,*a):
+    global res,mkdCt
+    if n < 1:
         s,e = a
+        if n != -1 and (n,s,e) in cache:
+            return cache[(n,s,e)]
     if n == 1:
-        leaf,pos,pack = a
-    if n == 2:      # used A
-        li,st,ct = a
-    if n == 3:      # used A
-        leaf,li = a
+        leaf,p,pk = a
     cur = m if n > 0 else s
-    que = deque([cur])
-    mkd,step = {cur:-1},{cur:-1}
+    q = PriorityQueue() if n > 0 else []
+    mkd = {cur:[cur]}
+    g = {cur:0}    
+    def heu(m):
+        ct = 0         
+        if t == 2:
+            return 0   
+        if p != -1:
+            e = p
+            di = {}
+            for i in range(size):
+                if m[i] == pk and not fx[i]:
+                    di[i] = len([i for i in src(0,m,i,e) if m[i] not in '0'+pk])            
+            s = min(di,key=lambda n:di[n])
+            rp = di[s]            
+            ct += 10000 * rp
+            r2 = src(-2,m,p,-1)
+            isDead = len(r2) > 1
+        for i in range(size):
+            if m[i] not in '0x':
+                y1,x1 = divmod(i,sx)
+                y2,x2 = divmod(p,sx)
+                dst = (abs(y1 - y2) + abs(x1 - x2)) + 1
+                ct += (100 * (size - dst) if isDead and i in r2 else -dst) if p != -1 else dst
+        if p != -1 and not rp:
+            ct = -2 ** (30 if m[p] == pk else 20)        
+        return ct
+    put = lambda cur: q.put((g[cur] + heu(cur), cur)) if n > 0 else q.append(cur)
+    get = lambda : q.get()[1] if n > 0 else q.pop(0)
+    put(cur)
     while 1:
-        cur = que.popleft()
-        if n == -1 and m[cur] != '0':
+        if n == -2 and len(q) > 1:      # default
+            break
+        if n in [0,-1] and not q:      # default
+            return mkd
+        cur = get()
+        if n > 0:
+            mkdCt += 1
+        if n == -3 and m[cur] not in '0' + e:
             break
         if n == 0 and cur == e:
             break
         if n == 1:
-            cur1 = cur[0] if t == 3 else cur
-            if (pos == -1 and cur1 == leaf) or (pos != -1 and cur1[pos] == pack):
+            if (p == -1 and cur == leaf) or (p != -1 and cur[p] == pk):
                 break
-        if n == 2:            
-            if st[:ct] in ''.join([cur[i] for i in li if cur[i] != '0'])[:ct]:
-                break
-        if n == 3 and all([cur[i] == leaf[i] for i in li]):
-            break
-        for i,j in exp(n,cur if n > 0 else m,cur):
+        for i,j in exp(n,cur if n > 0 else m,cur,leaf if n == 1 else -1):
             if i not in mkd:
-                que.append(i)
-                mkd[i],step[i] = cur,j
-    mkd[-2] = cur
-    path = [step[cur]]
-    while mkd[cur] != -1:
-        cur = mkd[cur]
-        path.append(step[cur])
+                mkd[i] = mkd[cur] + [j]
+                g[i] = g[cur] + 1
+                put(i)
+    res1 = mkd[cur]
     if n > 0:
-        res += path[::-1][1:]
-        return mkd[-2]
-    return path[::-1][1:]
-    
-def sort(m,leaf,e,p=-1):
-    pack = leaf[e] if p == -1 else p
-    b = len(exp(0,m,e)) == 1 
-    if b:
-        m = bfs(1,m,leaf,e,'0')
-    s = m.index(pack)
-    r = bfs(0,m,s,e)
-    if b:
-        fix.append(e)
-    for i in r:
-        if b and i in fix:
-            fix.remove(i)
-        m = bfs(1,m,leaf,i,pack)
-    fix.append(e)
-    return m
+        res += res1[1:]
+        return cur
+    if n != -1:
+        cache[(n,s,e)] = res1
+    return res1
 
-def main(g_t,m,*a):    
-    global t,sy,sx,sz,size,fix,res,cache
+def main(g_t,m,*a):
+    global t,sy,sx,size,fx,fxli,cache,res
     t = g_t
-    sy,sx,sz = [[5,5,1],[4,4,1],[1,8,1],[1,2,4]][t]
+    sy,sx = [[5,5],[4,4],[4,2]][t]
     size = sy * sx
-    fix = []
+    fx = {i:0 for i in range(size)}
+    fxli = lambda li,n: fx.update({i:n for i in li})
+    cache = {}
     res = []
-    cache = {}      # cache reset
-    if t == 0:
-        leaf, = a
-        hold = {}
-        unhold = []
-        leafc = leaf[:]
-        for i in [1,3,5,9,15,19,21]:
-            pack = leaf[i]
-            if pack == '0' or pack in hold:
-                r = [i]+bfs(-1,leafc,i)
-                pos = r[-1]
-                pack = leafc[pos]
-                hold[pack] = r
-                unhold.append(pack)
-                leafc = list(leafc)
-                leafc[pos] = '0'
-                leafc = ''.join(leafc)
-            m = sort(m,None,i,pack)        
-        li1 = [6,7,8,13,18,17,16,11]
-        li2 = [6,7,8,13,18,23,17,16,11]
-        st = ''.join([leaf[i] for i in li2 if leaf[i] not in unhold + ['0']])
+    if t < 2:
+        leaf,=a
+        li = [[1,3,5,9,15,19,7,6,8,11,13],[1,2,4,8,7,11,13,14]][t]
+        hd,hdr = [],{}
+        leaf1 = list(leaf[:])
+        for i in li:
+            pk = leaf1[i]
+            if pk == '0':
+                r = src(-3,leaf1,i,''.join(hd))
+                pk = leaf1[r[-1]]
+                hd.append(pk)
+                hdr[pk] = r
+                leaf1[r[-1]],leaf1[i] = '0',pk
+            m = src(1,m,''.join(leaf1),i,pk)
+            fx[i] = 1
+        m = src(1,m,''.join(leaf1),-1,-1)
+        for i in hd[::-1]:
+            di = {'a':10,'b':11,'c':12}
+            res.append([hdr[i],di[pk] if pk in di else int(pk)])
+    else:
+        li = sorted(map(int,m))[2:]
+        leaf = ''.join([str(li[i]) for i in [4,5,2,3,0,1]])+'00'
+
         for i in range(6):
-            m = bfs(2,m,li1,st,i)        
-        fix.append(23)
-        if leaf[23] not in unhold + ['0']:
-            m = bfs(1,m,leaf,18,leaf[23])
-            m,step = exc(m,23,18)
-            res.append(step)
-        m = bfs(3,m,leaf,[leaf.index(m[i]) for i in li1 if m[i] != '0'])
-        for pack in unhold[::-1]:
-            r = hold[pack]            
-            m,step = exc(m,r[-1],r[0],li=r)
-            res.append(step)
-    if t == 1:
-        leaf, = a
-        for i in [1,2,7,11,14,13,8,4]:
-            m = sort(m,leaf,i)
-    if t == 2:
-        li = [1,0,2,3,4,5,6,7,8]
-        for i in range(8):
-            j = (4 + li[i]) % 8
-            print(j,str(i+1))
-            m = bfs(1,m,None,j,str(i+1))
-            fix.append(j)
-        res.append([[4,5],2,1])
-    if t == 3:
-        li = [sorted([i for i in m if i in st]) for st in ['2468','1357']]
-        leaf = ['0']*8
-        for i in range(2):
-            li1 = li[i][::-1]
-            for j in range(3):
-                leaf[i+(2 * j)] = li1[j]
-        m = (m,('0','0'))
-        for i in range(6):
-            m = bfs(1,m,None,i,leaf[i])
-            fix.append(i)
+            m = src(1,m,leaf,i,leaf[i])
+            fx[i] = 1
+
     return res
 
 if __name__ == '__main__':
-    # t,m,leaf = 0,'x3x5x190a2xcx0x60b07x8x4x','x5x6x20b97x0xcx30a04x1x8x'
-    # t,m,leaf = 1,'x10x05483726x00x','x21x30086004x75x'
-    # t,m = 2,'64728153'
-    # t,m = 3,'52631400'
-    
-    # t,m,leaf = 0,'x3x5x190a2xcx0x60b07x8x4x', 'x5x6x21b97x8xcx30a04x0x0x'
-    # t,m,leaf = 1,'x10x05483726x00x', 'x21x30086004x75x'
-    # t,m = 2,'64728153'
-    t,m = 3,'52631400'
 
-    ts = time()    
-    res = main(t,m) if t > 1 else main(t,m,leaf)
+    t,m1,m2 = 0,'x3x5x190a2xcx0x60b07x8x4x','x5x6x21b97x8xcx30a04x0x0x'
+    # t,m1,m2 = 1,'x10x05483726x00x','x21x30086004x75x'
+    # t,m = 2,'12''34''56''00'
+
+    ts = time()
+    res = main(t,m1,m2) if t < 2 else main(t,m)
     te = time() - ts
     print(res)
-    print("{}step, idle {}s(dart {}m {}s) \n".
-    format(len(res),round(te,3),int((te*250)//60),int((te*250)%60)))
-
-
+    print('visit :',mkdCt)
+    print("{}step, idle {}s(DART-Studio {}m {}s) \n".format(len(res),round(te,3),int((te*250)//60),int((te*250)%60)))
